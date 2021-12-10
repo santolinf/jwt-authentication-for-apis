0f8c4f4d5608245c258e3274bbe3495a540748a4
Frank Santolin <frank@cadha.com>
2021-12-10 16:41:00 +0000

    initial revision

---
+++ src/main/java/com/manning/liveproject/jwt/authenticationforapi/AuthenticationForApiApplication.java
@@ -0,0 +1,13 @@
+package com.manning.liveproject.jwt.authenticationforapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationForApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationForApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manning/liveproject/jwt/authenticationforapi/AuthenticationForApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.liveproject.jwt.authenticationforapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationForApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
